510a3cebff6c039dc1d5d6da04a231601fb9bc12
RVillanuevaRVC <75404849+RVillanuevaRVC@users.noreply.github.com>
2020-12-05 17:14:13 +0700

    Update like_button.js

--- like_button.js
+++ like_button.js
@@ -2,13 +2,16 @@
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
-  constructor(props) {
+class LikeButton extends React.Component 
+{
+  constructor(props) 
+  {
     super(props);
     this.state = { liked: false };
   }
 
-  render() {
+  render() 
+  {
     if (this.state.liked) {
       return 'You liked this.';
     }
